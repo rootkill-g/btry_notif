4ce56d34a1c64942f8bbf056d455ce4a7e810e98
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2021-12-28 17:55:50 +0530

    btry_notif

--- btry_notif.sh
+++ btry_notif.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 battery_level=$(acpi -b | grep -oP '[0-9]+(?=%)')
 is_discharging=$(acpi -b | grep -c "Discharging")
-if [ $battery_level -le 99 ] && [ is_discharging == 1 ] ; then
+if [ $battery_level -le 65 ] && [ is_discharging == 1 ] ; then
   notify-send "Battery $battery_level! Plug-in ASAP"
 fi
